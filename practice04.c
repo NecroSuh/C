825f8b58f876f0d7bb95021e69211597293fdeff
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-07 21:47:47 +0900

    Add files via upload

---
+++ practice04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int floor;
+	printf("몇 층으로 앃겠는가?: ");
+	scanf("%d", &floor);
+	for (int i = 0; i < floor; i++) {
+		for (int j = i; j < floor - 1; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < i * 2 + 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
