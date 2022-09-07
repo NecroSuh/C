af8a6499a9af3d332cd85f4e059079ae8cb26750
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-09-07 23:39:29 +0900

    Add files via upload

---
+++ an22-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct employee {
+	char name[20];
+	char pid[20];
+	int salary;
+};
+
+int main() {
+	struct employee emp;
+	printf("이름 : ");
+	scanf("%s", emp.name);
+	printf("주민번호 : ");
+	scanf("%s", emp.pid);
+	printf("급여 : ");
+	scanf("%d", &emp.salary);
+
+	printf("이름 : %s\n", emp.name);
+	printf("주민번호 : %s\n", emp.pid);
+	printf("급여 : %d\n", emp.salary);
+
+	return 0;
+}
