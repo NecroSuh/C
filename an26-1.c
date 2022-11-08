49c5037e355c4a740a7e3cbc8c24fe697e76e646
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-11-08 23:23:20 +0900

    Add files via upload

---
+++ an26-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define ADD(x,y,z) ((x)+(y)+(z))
+#define MUL(x,y,z) ((x)*(y)*(z))
+
+int main() {
+	printf("3+4+5 = %d\n", ADD(3, 4, 5));
+	printf("3X4X5 = %d\n", MUL(3, 4, 5));
+	return 0;
+}
